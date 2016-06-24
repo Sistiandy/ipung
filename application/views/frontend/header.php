aa41f686dfcc3d6323f72fbbc82850f663d60ef4
Muhammad Nazih <nazih@artikulpi.com>
2016-06-24 14:41:11 +0700

    Design:MOdifikasi tampilan font dan style link

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -1,85 +1,84 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
 
-        <title>Ipung Salon</title>
-        <link rel="icon" href="<?php echo media_url() ?>/images/slide/logo_salon.png" type="image/x-icon">
-        <!-- Loading third party fonts -->
-        <link href="<?php echo media_url() ?>/fonts/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-        <!-- Loading main css file -->
-        <link rel="stylesheet" href="<?php echo media_url() ?>/css/bootstrap.min.css" type="text/css">
-        <link rel="stylesheet" href="<?php echo media_url() ?>/css/animate.min.css" type="text/css">
-        <link rel="stylesheet" href="<?php echo media_url() ?>/css/ionicons.min.css" type="text/css">
-        <link rel="stylesheet" href="<?php echo media_url() ?>/css/style.css" type="text/css">
+    <title>Ipung Salon</title>
+    <link rel="icon" href="<?php echo media_url() ?>/images/slide/logo_salon.png" type="image/x-icon">
+    <!-- Loading third party fonts -->
+    <link href="<?php echo media_url() ?>/fonts/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Loading main css file -->
+    <link rel="stylesheet" href="<?php echo media_url() ?>/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo media_url() ?>/css/animate.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo media_url() ?>/css/ionicons.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo media_url() ?>/css/style.css" type="text/css">
+</head>
 
-    </head>
+<body class="slider-collapse">
 
-    <body class="slider-collapse">
-
-        <nav class="navbar navbar-findcond navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Salon IPung</a>
-                </div>
-                <div class="collapse navbar-collapse" id="navbar">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="ion-email"></i> Contact Person <span class="badge">0</span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#"><i class="fa fa-fw fa-tag"></i> <span class="badge">BBM</span> 098675 </a></li>
-                                <li><a href="#"><i class="fa fa-fw fa-thumbs-o-up"></i> <span class="badge">WA</span> 089602824560</a></li>
-                                <li><a href="#"><i class="fa fa-fw fa-thumbs-o-up"></i> <span class="badge">SMS</span> 08962876288</a></li>
-                                <li><a href="#"><i class="fa fa-fw fa-thumbs-o-up"></i> <span class="badge">Ig</span>  @Ipung</a></li>
-                            </ul>
-                        </li>
-                        <li class="active"><a href="#"> +1 344 5345 5345  <span class="sr-only">(current)</span></a></li>
-                    </ul>
-                    <form class="navbar-form navbar-right search-form" role="search">
-                        <input type="text" class="form-control" placeholder="Search" />
-                    </form>
-                </div>
+    <nav class="navbar navbar-findcond navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Salon IPung</a>
             </div>
-        </nav>
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="ion-email"></i> Contact Person <span class="badge">0</span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#"><i class="fa fa-fw fa-tag"></i> <span class="badge">BBM</span> 098675 </a></li>
+                            <li><a href="#"><i class="fa fa-fw fa-thumbs-o-up"></i> <span class="badge">WA</span> 089602824560</a></li>
+                            <li><a href="#"><i class="fa fa-fw fa-thumbs-o-up"></i> <span class="badge">SMS</span> 08962876288</a></li>
+                            <li><a href="#"><i class="fa fa-fw fa-thumbs-o-up"></i> <span class="badge">Ig</span>  @Ipung</a></li>
+                        </ul>
+                    </li>
+                    <li class="active"><a href="#"> +1 344 5345 5345  <span class="sr-only">(current)</span></a></li>
+                </ul>
+                <form class="navbar-form navbar-right search-form" role="search">
+                    <input type="text" class="form-control" placeholder="Search" />
+                </form>
+            </div>
+        </div>
+    </nav>
 
-        <div id="site-content">
+    <div id="site-content">
 
-            <header class="site-header wow fadeInDown">
-                <div class="container">
-                    <div class="header-content">
-                        <div class="branding">
-                            <img src="<?php echo media_url() ?>/images/slide/logo_salon.png" alt="Company Name" class="logo">
-                            <h1 class="site-title"><a href="index.html"></a></h1>
-                            <small class="site-description"></small>
-                        </div>
+        <header class="site-header wow fadeInDown">
+            <div class="container">
+                <div class="header-content">
+                    <div class="branding">
+                        <img src="<?php echo media_url() ?>/images/slide/logo_salon.png" alt="Company Name" class="logo">
+                        <h1 class="site-title"><a href="index.html"></a></h1>
+                        <small class="site-description"></small>
+                    </div>
 
-                        <nav class="main-navigation">
-                            <button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
-                            <ul class="menu">
-                                <li class="menu-item"><a href="about-us.html">Tentang</a></li>
-                                <li class="menu-item"><a href="our-offer.html">Produk</a></li>
-                                <li class="menu-item"><a href="customer-protection.html">Cabang</a></li>
-                                <li class="menu-item"><a href="galeri.html">Galeri</a></li>
-                                <li class="menu-item"><a href="contact.html">Kontak</a></li>
-                            </ul>
-                        </nav>
+                    <nav class="main-navigation">
+                        <button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
+                        <ul class="menu">
+                            <li class="menu-item"><a href="about-us.html">Tentang</a></li>
+                            <li class="menu-item"><a href="our-offer.html">Produk</a></li>
+                            <li class="menu-item"><a href="customer-protection.html">Cabang</a></li>
+                            <li class="menu-item"><a href="galeri.html">Galeri</a></li>
+                            <li class="menu-item"><a href="contact.html">Kontak</a></li>
+                        </ul>
+                    </nav>
 
-                        <div class="social-links">
-                            <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-                            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-                            <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
-                            <a href="#" class="pinterest"><i class="fa fa-pinterest"></i></a>
-                        </div>
+                    <div class="social-links">
+                        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+                        <a href="#" class="pinterest"><i class="fa fa-pinterest"></i></a>
                     </div>
                 </div>
-            </header> <!-- .site-header -->
+            </div>
+        </header> <!-- .site-header -->
 
-            <main class="content">
+        <main class="content">
