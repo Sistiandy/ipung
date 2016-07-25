b7ecd224fd6d7ac28d04e623e73e7c4c11e269e2
Muhammad Nazih <nazih@artikulpi.com>
2016-07-25 15:18:55 +0700

    Deisgn:Perbaikan deskripsi about di footer ipung salon

--- application/views/frontend/footer.php
+++ application/views/frontend/footer.php
@@ -7,68 +7,69 @@
                 <div class="col-md-3 col-sm-6">
                     <div class="widget">
                         <h3 class="widget-title">About us</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus animi asperiores magnam ducimus laboriosam soluta, odio doloribus, voluptas numquam facilis consectetur nam in repudiandae commodi odit iste sed doloremque repellat.</p>
+                        <p>ipung-cosmetics.com adalah online makeup store yang berdiri sejak 2015 dengan misi menyediakan kosmetik dan alat-alat makeup dengan harga yang sangat terjangkau dan tentunya berkualitas.
+                            Dengan berdirinya ipung-cosmetics.com ini kami berharap dapat memberikan pelayanan yang terbaik dalam memenuhi kebutuhan akan kosmetik dan alat-alat make up untuk semua customer kami.</p>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <div class="widget">
-                        <h3 class="widget-title">Helpful Links</h3>
-                        <ul class="list-arrow">
-                            <li><a href="#">Labore et dolore magnam</a></li>
-                            <li><a href="#">Dolore magnam</a></li>
-                            <li><a href="#">Magnam Labore et</a></li>
-                            <li><a href="#">Dolore mabore magnam</a></li>
-                            <li><a href="#">Et dolore magnam</a></li>
-                        </ul>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="widget">
+                            <h3 class="widget-title">Helpful Links</h3>
+                            <ul class="list-arrow">
+                                <li><a href="#">Labore et dolore magnam</a></li>
+                                <li><a href="#">Dolore magnam</a></li>
+                                <li><a href="#">Magnam Labore et</a></li>
+                                <li><a href="#">Dolore mabore magnam</a></li>
+                                <li><a href="#">Et dolore magnam</a></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <div class="widget">
-                        <h3 class="widget-title">Helpful Links</h3>
-                        <ul class="list-arrow">
-                            <li><a href="#">Labore et dolore magnam</a></li>
-                            <li><a href="#">Dolore magnam</a></li>
-                            <li><a href="#">Magnam Labore et</a></li>
-                            <li><a href="#">Dolore mabore magnam</a></li>
-                            <li><a href="#">Et dolore magnam</a></li>
-                        </ul>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="widget">
+                            <h3 class="widget-title">Helpful Links</h3>
+                            <ul class="list-arrow">
+                                <li><a href="#">Labore et dolore magnam</a></li>
+                                <li><a href="#">Dolore magnam</a></li>
+                                <li><a href="#">Magnam Labore et</a></li>
+                                <li><a href="#">Dolore mabore magnam</a></li>
+                                <li><a href="#">Et dolore magnam</a></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <div class="widget widget-customer-info">
-                        <h3 class="widget-title">Customer Service</h3>
-                        <img src="<?php echo media_url() ?>/images/slide/produk/cs.png" alt="" width="20%" class="pull-left">
-                        <div class="cs-info">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim voluptates pariatur vero.</p>
-                            <p>+1 421 458 321 <br> <a href="mailto:nazihbopas@gmail.com">nazihbopas@gmail.com</a></p>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="widget widget-customer-info">
+                            <h3 class="widget-title">Customer Service</h3>
+                            <img src="<?php echo media_url() ?>/images/slide/produk/cs.png" alt="" width="20%" class="pull-left">
+                            <div class="cs-info">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim voluptates pariatur vero.</p>
+                                <p>+1 421 458 321 <br> <a href="mailto:nazihbopas@gmail.com">nazihbopas@gmail.com</a></p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="footer-bottom">
-        <div class="container">
-            <div class="branding pull-left">
-                <img src="<?php echo media_url() ?>/images/slide/logo_salon.png" alt="Company Name" class="logo">
-                <h1 class="site-title"><a href="index.html">Salon Ipung</a></h1>
-                <small class="site-description">Skin Beauty Center</small>
-            </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="branding pull-left">
+                    <img src="<?php echo media_url() ?>/images/slide/logo_salon.png" alt="Company Name" class="logo">
+                    <h1 class="site-title"><a href="index.html">Salon Ipung</a></h1>
+                    <small class="site-description">Skin Beauty Center</small>
+                </div>
 
-            <div class="contact-links pull-right">
-                <a href="https://goo.gl/maps/oQKxg" target="_blank"><i class="fa fa-map-marker"></i> 983 Avenue Street, New York</a>
-                <a href="tel:+134453455345"><i class="fa fa-phone"></i> +896-0282-4560</a>
-                <a href="mailto:nazihbopas@gmail.com"><i class="fa fa-envelope"></i> nazihbopas@gmail.com</a>
+                <div class="contact-links pull-right">
+                    <a href="https://goo.gl/maps/oQKxg" target="_blank"><i class="fa fa-map-marker"></i> 983 Avenue Street, New York</a>
+                    <a href="tel:+134453455345"><i class="fa fa-phone"></i> +896-0282-4560</a>
+                    <a href="mailto:nazihbopas@gmail.com"><i class="fa fa-envelope"></i> nazihbopas@gmail.com</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="colophon">
-        <div class="container">
-            <p class="copy">Copyright 2016 Ipung Cosmetics, All right reserved.</p>
+        <div class="colophon">
+            <div class="container">
+                <p class="copy">Copyright 2016 Ipung Cosmetics, All right reserved.</p>
+            </div>
         </div>
-    </div>
-</footer> <!-- .site-footer -->
+    </footer> <!-- .site-footer -->
 
 </div> <!-- #site-content -->
 
